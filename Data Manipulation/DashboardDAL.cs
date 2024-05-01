e4308cf7d35d4a271720107cc63af2c2841258f7
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-05-01 18:18:15 +0300

    dashboard count functionality for DB

---
+++ Data Manipulation/DashboardDAL.cs
@@ -0,0 +1,93 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Employees_Management_System.Data_Manipulation
+{
+    internal class DashboardDAL
+    {
+        public int CountAllEmployees()
+        {
+            int count = 0;
+
+            using(MySqlConnection conn = new MySqlConnection(Program.GetConnectionString()))
+            {
+                try
+                {
+                    string sql = "SELECT COUNT(*) FROM employees";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    conn.Open();
+                    count = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+
+            return count;
+        }
+
+        public int CountInactiveEmployees()
+        {
+            int count = 0;
+
+            using(MySqlConnection conn = new MySqlConnection(Program.GetConnectionString()))
+            {
+                try
+                {
+                    //string sql = "SELECT COUNT(*) FROM employees WHERE status = 'Inactive'";
+                    string sql = "SELECT COUNT(*) FROM employees WHERE status LIKE 'Inactive%'";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    conn.Open();
+                    count = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+
+            return count;
+        }
+
+        public int CountActiveEmployee()
+        {
+            int count = 0;
+
+            using(MySqlConnection conn = new MySqlConnection(Program.GetConnectionString()))
+            {
+                try
+                {
+                    string sql = "SELECT COUNT(*) FROM employees WHERE salary LIKE 'Inactive%'";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    conn.Open();
+                    count = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+
+            return count;
+        }
+    }
+}
